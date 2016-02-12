cf6df66f76111b144cf86d79a6db9d984a7e918d
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-02-12 15:28:37 -0800

    Implement counter

--- counter.go
+++ counter.go
@@ -1,5 +1,39 @@
 package main
 
-func CountLines(goPath string) (int, error) {
-	return 0, nil
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func CountLines(importPath string) (int, error) {
+	rootDir := filepath.Clean(filepath.Join(os.Getenv("GOPATH"), "src", importPath))
+
+	lineCount := 0
+
+	err := filepath.Walk(rootDir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() || !strings.HasSuffix(path, ".go") {
+				return nil
+			}
+
+			linesInFile, err := countLinesInFile(path)
+			lineCount += linesInFile
+			return err
+		})
+
+	return lineCount, err
+}
+
+func countLinesInFile(filepath string) (int, error) {
+	fileContents, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return 0, err
+	}
+	return bytes.Count(fileContents, []byte("\n")), nil
 }
