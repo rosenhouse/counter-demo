bf5cdc5e83a5a77dfd5b76bd2eb4bd52a7cec8e4
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-02-22 21:13:03 -0800

    fix unit test of lines handler

--- handlers/lines.go
+++ handlers/lines.go
@@ -1,10 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
+
+type counter interface {
+	Count(packagePath string) (int, error)
+}
 
 type Lines struct {
+	Counter counter
 }
 
 func (h *Lines) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	pkgPath := strings.TrimPrefix(req.URL.Path, "/lines/")
+	h.Counter.Count(pkgPath)
 	resp.Write([]byte(`{"lines": 42}`))
 }
