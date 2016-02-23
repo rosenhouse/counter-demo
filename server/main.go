ad66ad5e25e0702329c9b0b68183bbb033deae60
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-02-22 23:21:21 -0800

    wire up real handler, system test passes

--- server/main.go
+++ server/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/rosenhouse/counter-demo/counters"
+	"github.com/rosenhouse/counter-demo/handlers"
+)
 
 func main() {
+	goPath := os.Getenv("GOPATH")
+	if goPath == "" {
+		log.Fatalf("missing required env var GOPATH")
+	}
+
+	linesHandler := &handlers.Lines{
+		Counter: &counters.PackageLinesCounter{
+			GoPath:           goPath,
+			DirectoryLister:  &counters.DirectoryLister{},
+			FileLinesCounter: &counters.FileLinesCounter{},
+		},
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello"))
-	}))
+	mux.Handle("/lines/", linesHandler)
 	http.ListenAndServe(":8000", mux)
 }
